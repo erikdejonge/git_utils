5514d77c532eb653316ad18afe101e73f345c8e4
Erik de Jonge <erik@a8.nl>
2014-01-06 06:23:45 +0100

    Mon Jan  6 06:23:44 CET 2014

--- commit.sh
+++ commit.sh
@@ -1,4 +1,8 @@
-rm nohup.out
+if [ -b "nohup.out" ]
+then
+  rm nohup.out
+fi
+
 nohup python /Users/rabshakeh/workspace/git_utils/commit_all_git_folders.py&&cat nohup.out &
 
 
--- push.sh
+++ push.sh
@@ -1,2 +1,9 @@
-nohup python /Users/rabshakeh/workspace/git_utils/push_all_git_folders.py&&cat nohup.out&&rm nohup.out &
-wait
+if [ -b "nohup.out" ]
+then
+  rm nohup.out
+fi
+
+nohup python /Users/rabshakeh/workspace/git_utils/push_all_git_folders.py&&cat nohup.out &
+
+
+
