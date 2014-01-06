02660fb75712574f5ff847d6b8f4ca50d1863755
Erik de Jonge <erik@a8.nl>
2014-01-06 06:20:49 +0100

    Mon Jan  6 06:20:45 CET 2014 commit and gc

--- commit.sh
+++ commit.sh
@@ -1,5 +1,5 @@
-
-nohup python /Users/rabshakeh/workspace/git_utils/commit_all_git_folders.py &
-wait
-cat nohup.out
 rm nohup.out
+nohup python /Users/rabshakeh/workspace/git_utils/commit_all_git_folders.py&&cat nohup.out &
+
+
+
