b4c8ea5bf134cc4a9a0f93391072d980144bae98
Erik de Jonge <erik@a8.nl>
2014-01-07 12:58:59 +0100

    Tue Jan  7 12:58:59 CET 2014

--- commitfast.sh
+++ commitfast.sh
@@ -1,2 +1,3 @@
 python /Users/rabshakeh/workspace/git_utils/commit_all_git_folders_fast.py
+nohup tar -cf /Users/rabshakeh/workspace/cryptobox/idea.tar /Users/rabshakeh/workspace/cryptobox/.idea >/dev/null 2>&1 
 wait
