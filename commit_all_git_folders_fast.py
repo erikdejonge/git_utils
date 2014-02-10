57ab2415cfba12a1aafe69183f9002fe60229211
Erik de Jonge <erik@a8.nl>
2014-02-10 13:13:44 +0100

    Mon Feb 10 13:13:44 CET 2014

--- commit_all_git_folders_fast.py
+++ commit_all_git_folders_fast.py
@@ -41,11 +41,11 @@ def main():
     msg = os.popen("date").read().strip()
     procs = []
     for folder in dir_list:
-        print folder
+        
         if len([x for x in [x in folder for x in excludes] if x]) == 0:
             p = subprocess.Popen(["/usr/local/bin/git", "commit", "-am",  msg], stdout=subprocess.PIPE, cwd=folder)
             procs.append({"folder":folder, "proc":p})
-            
+
     for d in procs:
         p = d["proc"]
         p.wait()
