735734a6564f546c4dda9eff104f63da1b17fcec
Erik de Jonge <erik@a8.nl>
2014-03-05 11:19:10 +0100

    Wed Mar  5 11:19:03 CET 2014 commit and gc

--- pull_all_git_folders.py
+++ pull_all_git_folders.py
@@ -14,10 +14,13 @@ def find_git_repos(arg, directory, files):
     @type files: list
     """
     files = files
-    disp = directory[:100]
+    disp = directory.replace("/Users/rabshakeh/workspace/", "")[:100]
+
     if len(disp) < 100:
         disp += " " * (100 - len(disp))
-    print disp, "\r",
+
+    sys.stdout.write(disp+"\r")
+    sys.stdout.flush()
     git_dir = os.path.join(directory, ".git")
 
     if os.path.exists(git_dir):
@@ -31,41 +34,36 @@ def main():
     excludes = []
     """ check all folders and pull all from the server """
     dir_list = []
-    print "scanning"
     os.path.walk("/Users/rabshakeh/workspace", find_git_repos, dir_list)
     currdir = "/"
-    print ""
-    print "pulling"
+
     dir_list = [os.path.join(currdir, x.lstrip("./")) for x in dir_list]
     procs = []
     last_sleep = 0
 
     for folder in dir_list:
         if len([x for x in [x in folder for x in excludes] if x]) == 0:
-
-            p = subprocess.Popen(["/usr/local/bin/git", "pull"], stdout=subprocess.PIPE, cwd=folder)
+            p = subprocess.Popen(["/usr/local/bin/git", "pull"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=folder)
             procs.append({"folder": folder, "proc": p})
             if len(procs) % 15 == 0 and last_sleep != len(procs):
                 #print "sleep", len(procs), last_sleep
                 time.sleep(1)
                 last_sleep = len(procs)
-
             #print folder, p.wait()
-
     for d in procs:
         p = d["proc"]
+        sys.stdout.write(d["folder"])
+        sys.stdout.flush()
         p.wait()
-
         output = p.stdout.read()
+        output += p.stderr.read()
 
         if "Already up-to-date" in output:
-            print d["folder"], "up-to-date"
+            sys.stdout.write(".. ok\n")
             sys.stdout.flush()
         else:
-            print "pull_all_git_folders.py:55"
-            print "pull_all_git_folders.py:56", d["folder"]
-            print "pull_all_git_folders.py:57", output
-
+            sys.stdout.write("\n"+output+"\n")
+            sys.stdout.flush()
 
 if __name__ == "__main__":
     main()
