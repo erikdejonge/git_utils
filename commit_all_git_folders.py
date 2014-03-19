e8494e00e590fb0a0ce5a71486cf95c7feb755f6
Erik de Jonge <erik@a8.nl>
2014-03-19 12:47:22 +0100

    Wed Mar 19 12:47:15 CET 2014 commit and gc

--- commit_all_git_folders.py
+++ commit_all_git_folders.py
@@ -10,14 +10,18 @@ import sys
 import os
 import subprocess
 import cPickle as pickle
+lastdirvisited = ""
 
 def find_git_repos(arg, directory, files):
     """ find the git repositories """
-
+    global lastdirvisited
     files = files
     ds = directory.split("/")
     if len(ds)>3:
-        print ds[len(ds)-4]+"/"+ds[len(ds)-3]+"/"+ds[len(ds)-2]+"/"+ds[len(ds)-1]
+        printpath = ds[len(ds)-4]+"/"+ds[len(ds)-3]+"/"+ds[len(ds)-2]+"/"+ds[len(ds)-1]
+        if lastdirvisited!=printpath:
+            print ds[len(ds)-4]+"/"+ds[len(ds)-3]+"/"+ds[len(ds)-2]+"/"+ds[len(ds)-1]
+            lastdirvisited = printpath
     git_dir = os.path.join(directory, ".git")
     if os.path.exists(git_dir):
         arg.append(directory)
