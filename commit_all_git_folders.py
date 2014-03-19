a5afbdc6bc0cbb0d40439f87bc9cd489d54c3466
Erik de Jonge <erik@a8.nl>
2014-03-19 12:50:15 +0100

    Wed Mar 19 12:50:10 CET 2014 commit and gc

--- commit_all_git_folders.py
+++ commit_all_git_folders.py
@@ -17,11 +17,11 @@ def find_git_repos(arg, directory, files):
     global lastdirvisited
     files = files
     ds = directory.split("/")
-    if len(ds)>3:
-        printpath = ds[len(ds)-4]+"/"+ds[len(ds)-3]+"/"+ds[len(ds)-2]+"/"+ds[len(ds)-1]
-        if str(ds[len(ds)-4]).strip() not in lastdirvisited:
+    if len(ds)>4:
+        printpath = ds[len(ds)-5]+"/"+ds[len(ds)-4]+"/"+ds[len(ds)-3]+"/"+ds[len(ds)-2]+"/"+ds[len(ds)-1]
+        if str(ds[len(ds)-5]).strip() not in lastdirvisited:
             print printpath
-            lastdirvisited[str(ds[len(ds)-4]).strip()] = True
+            lastdirvisited[str(ds[len(ds)-5]).strip()] = True
     git_dir = os.path.join(directory, ".git")
     if os.path.exists(git_dir):
         arg.append(directory)
