413040acea9a797e19160f56f73b8e2791960286
Erik de Jonge <erik@a8.nl>
2014-03-20 14:58:50 +0100

    Thu Mar 20 14:58:50 CET 2014

--- pull_all_git_folders.py
+++ pull_all_git_folders.py
@@ -19,25 +19,25 @@ def main():
         dir_list = cPickle.load(open(dfp))
     else:
         raise RuntimeError("Cannot find /Users/rabshakeh/workspace/git_utils/gitdirlist.pickle")
-    print
+
     procs = []
 
     for folder in dir_list:
         p = subprocess.Popen(["/usr/local/bin/git", "pull"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=folder)
         procs.append({"folder": folder, "proc": p})
         print "\033[92mpull " + os.path.basename(folder) + "\033[0m"
-        if len(procs) > 5:
 
-            for d in procs:
-                p = d["proc"]
-                p.wait()
-                output = p.stdout.read()
-                output += p.stderr.read()
-
-                if "Already up-to-date" in output:
-                    pass
-                else:
-                    print "\033[93m" + output.strip() + "\033[0m"
+    for d in procs:
+        p = d["proc"]
+        p.wait()
+        output = p.stdout.read()
+        output = output.strip()
+        output += p.stderr.read()
+        output = output.strip()
+        if "Already up-to-date" in output:
+            pass
+        else:
+            print "\033[93m" + output.strip() + "\033[0m"
 
 
 if __name__ == "__main__":
