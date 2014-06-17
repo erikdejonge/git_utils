8fd7954dadc55ad47f4090757a742b74a50b3345
Erik de Jonge <erik@a8.nl>
2014-06-17 15:25:01 +0200

    -

--- commit_all_git_folders_fast.py
+++ commit_all_git_folders_fast.py
@@ -31,16 +31,9 @@ def main():
     for folder in dir_list:
         if len([x for x in [x in folder for x in excludes] if x]) == 0:
             print folder
-            p = subprocess.Popen(["/usr/bin/git", "commit", "-am",  msg], stdout=subprocess.PIPE, cwd=folder)
+            p = subprocess.Popen(["/usr/bin/git", "commit", "-am",  msg], cwd=folder)
             procs.append({"folder":folder, "proc":p})
             p.wait()
-            output = p.stdout.read()
-            if "nothing to commit" in output:
-                sys.stdout.write(".")
-                sys.stdout.flush()
-            else:
-                print
-                print "\033[92mcommit "+os.path.basename(folder)+"\033[0m\n\033[93m"+output.strip()+"\033[0m"
     print
 
 
