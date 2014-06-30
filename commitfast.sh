15d5f3e9d1e1438bd54d62c6b0b50af348f2b6de
Erik de Jonge <erik@a8.nl>
2014-06-30 11:47:28 +0200

    Mon Jun 30 11:47:28 CEST 2014

--- commitfast.sh
+++ commitfast.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+bash -c "cd /Users/rabshakeh/workspace/cryptobox/crypto_data&&git add databases/*;"
 python /Users/rabshakeh/workspace/git_utils/commit_all_git_folders_fast.py
-nohup tar -cf /Users/rabshakeh/workspace/cryptobox/idea.tar /Users/rabshakeh/workspace/cryptobox/.idea >/dev/null 2>&1 
+nohup tar -cf /Users/rabshakeh/workspace/cryptobox/idea.tar /Users/rabshakeh/workspace/cryptobox/.idea >/dev/null 2>&1
 wait
