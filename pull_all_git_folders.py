fc17fa2e9a1b0c7a6ee0ea125b1f8413781ae885
Erik de Jonge <erik@a8.nl>
2014-08-15 13:07:18 +0200

    Fri Aug 15 13:07:18 CEST 2014

--- pull_all_git_folders.py
+++ pull_all_git_folders.py
@@ -23,11 +23,11 @@ def main():
     procs = []
     cnt = 0
     for folder in dir_list:
-        p = subprocess.Popen(["/usr/local/bin/git", "pull"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=folder)
+        p = subprocess.Popen(["/usr/local/bin/git", "pull"], cwd=folder)
         procs.append({"folder": folder, "proc": p})
 
         print "\033[92mpull " + os.path.basename(folder) + "\033[0m"
-        if cnt > 2:
+        if cnt > 4:
             p.wait()
             cnt = 0
         else:
@@ -37,14 +37,14 @@ def main():
     for d in procs:
         p = d["proc"]
         p.wait()
-        output = p.stdout.read()
-        output = output.strip()
+        #output = p.stdout.read()
+        #output = output.strip()
         #output += p.stderr.read()
         #output = output.strip()
-        if "Already up-to-date" in output:
-            print "\033[90m"+os.path.basename(d["folder"])+" up-to-date\033[0m"
-        else:
-            print "\033[93m" + os.path.basename(d["folder"]) + " updated *\n" + output.strip() + "\033[0m"
+        #if "Already up-to-date" in output:
+        #    print "\033[90m"+os.path.basename(d["folder"])+" up-to-date\033[0m"
+        #else:
+        #    print "\033[93m" + os.path.basename(d["folder"]) + " updated *\n" + output.strip() + "\033[0m"
 
 
 if __name__ == "__main__":
