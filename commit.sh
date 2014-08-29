bcf2ad1cef1182156e7ccf442fb4ce4a04c8bccd
Erik de Jonge <erik@a8.nl>
2014-08-29 12:38:58 +0200

    Fri Aug 29 12:38:58 CEST 2014

--- commit.sh
+++ commit.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 rm /Users/rabshakeh/workspace/git_utils/exclude
-python /Users/rabshakeh/workspace/git_utils/commit_all_git_folders.py
+python /Users/rabshakeh/workspace/git_utils/make_exclude_dirs.py
 python /Users/rabshakeh/workspace/git_utils/commit_all_git_folders.py
 
 
