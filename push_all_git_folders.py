915ed06addab1bf0044231a6b23bba99df516a52
Erik de Jonge <erik@a8.nl>
2014-12-15 10:33:59 +0100

    Mon Dec 15 10:33:58 CET 2014

--- push_all_git_folders.py
+++ push_all_git_folders.py
@@ -53,7 +53,7 @@ def main():
 
                 p2 = subprocess.Popen(["/usr/local/bin/git", "push"], stderr=subprocess.PIPE, stdout=subprocess.PIPE, cwd=folder)
                 procs.append((folder, p2))
-                if cnt > 30:
+                if cnt > 8:
                     p2.communicate()
                     cnt = 0
                 else:
