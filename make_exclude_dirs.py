f00ff03ce5be02fd896ec902578b57486da0627d
Erik de Jonge <erik@a8.nl>
2015-02-20 15:00:31 +0100

    -

--- make_exclude_dirs.py
+++ make_exclude_dirs.py
@@ -13,10 +13,15 @@ def main():
     s = os.popen("ls " + os.path.expanduser("~") + "/workspace/github").read()
     l = s.strip().split("\n")
     f = open(os.path.expanduser("~") + "/workspace/git_utils/exclude_dirs", "w")
+    ws = os.popen("ls " + os.path.expanduser("~") + "/workspace").read()
+    wl = ws.strip().split("\n")
 
     for i in l:
-        f.write(i)
-        f.write("\n")
+        if i not in wl:
+            f.write(i)
+            f.write("\n")
+
+
 
 
 if __name__ == "__main__":
