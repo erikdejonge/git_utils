1d1ddb423a7a93f79ea66cab07faae4e391602fd
Erik de Jonge <erik@a8.nl>
2015-02-23 20:27:30 +0100

    workspace Monday 23 February 2015 (week:1 day:54), 20:27:29

--- commit_all_git_folders_fast.py
+++ commit_all_git_folders_fast.py
@@ -27,7 +27,7 @@ def main():
 
     if os.path.exists(os.path.expanduser("~") + "/workspace/.gitutilsexclude"):
         excludes.extend([os.path.join(os.path.expanduser("~") + "/workspace/.gitutilsexclude", x.strip()) for x in open(os.path.expanduser("~") + "/workspace/.gitutilsexclude").read().split("\n") if x.strip()])
-
+    print {1:excludes}
     dfp = os.path.expanduser(os.path.expanduser("~") + "/workspace/git_utils/gitdirlist.pickle")
 
     if os.path.exists(dfp):
