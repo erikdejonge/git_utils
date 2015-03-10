c3d0e5ff50e1cc4ac7cbfb842f3e2633b9fe0740
Erik de Jonge <erik@a8.nl>
2015-03-10 14:18:42 +0100

    pip Tuesday 10 March 2015 (week:10 day:69), 14:18:38

--- find_git_repos.py
+++ find_git_repos.py
@@ -1,5 +1,4 @@
 # coding=utf-8
-
 # -*- coding: utf-8 -*-
 """ git checking script """
 from __future__ import print_function
@@ -12,6 +11,7 @@ standard_library.install_aliases()
 
 import sys
 import os
+
 findcnt = 0
 
 
@@ -25,18 +25,20 @@ def find_git_repos(arg, directory, files):
     global findcnt
     findcnt += 1
 
-    if findcnt % 100 == 0:
-        sys.stdout.write(".")
-        sys.stdout.flush()
+    # if findcnt % 100 == 0:
+    #     sys.stdout.write(".")
+    #     sys.stdout.flush()
 
     git_dir = os.path.join(directory, ".git")
 
     if os.path.exists(git_dir):
-        sys.stdout.write("*")
-        sys.stdout.flush()
-
         config = open(git_dir + "/config").read().split("url =")[1].split("\n")[0].strip()
         arg.append((os.path.basename(directory), config))
+        if config.strip().startswith("http"):
+            print("\033[31m" + config + "\033[0m")
+        else:
+            print(config)
+
 
 
 def main():
@@ -44,11 +46,11 @@ def main():
     main
     """
     dir_list = []
+
     for root, dirlist, file in os.walk(os.path.expanduser("~") + "/workspace"):
         find_git_repos(dir_list, root, dirlist)
 
-    for i in dir_list:
-        print(i[1])
+
 
 
 if __name__ == "__main__":
