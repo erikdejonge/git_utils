8c0c208b6f1af0d41e94cbc2032213538d37b17c
Erik de Jonge <erik@a8.nl>
2015-03-20 09:45:02 +0100

    pip Friday 20 March 2015 (week:11 day:79), 09:45:01

--- push_all_git_folders.py
+++ push_all_git_folders.py
@@ -49,8 +49,10 @@ def main():
         dir_list = pickle.load(open(dfp, "rb"))
     else:
         dir_list = []
+
         for root, dirlist, file in os.walk("."):
             find_git_repos(dir_list, root, dirlist)
+
         currdir = os.popen("pwd").read().strip()
         dir_list = [os.path.join(currdir, x.lstrip("./")) for x in dir_list]
         pickle.dump(dir_list, open(dfp, "wb"))
@@ -64,36 +66,41 @@ def main():
                 sys.stdout.flush()
                 p = subprocess.Popen(["/usr/local/bin/git", "status"], stderr=subprocess.PIPE, stdout=subprocess.PIPE, cwd=folder)
                 output, se = p.communicate()
+
                 if output:
                     output = output.decode("utf-8")
+
                 if se:
                     se = se.decode("utf-8")
 
                 if "Your branch is ahead" in output or "have diverged" in output:
-                    print("\033[95mpush "+os.path.basename(folder)+"\033[0m")
+                    print("\033[95mpush " + os.path.basename(folder) + "\033[0m")
                     p2 = subprocess.Popen(["/usr/local/bin/git", "push"], stderr=subprocess.PIPE, stdout=subprocess.PIPE, cwd=folder)
                     procs.append((folder, p2))
 
-                    if cnt > 8:
-                        p2.communicate()
-                        cnt = 0
+                    if len(procs) > 4:
+                        try:
+                            p = procs.pop()
+                        except IndexError:
+                            p = None
+
+                        if p is not None:
+                            output, se = p[1].communicate()
+
+                            if output:
+                                output = output.decode("utf-8")
+
+                            if se:
+                                se = se.decode("utf-8")
+
+                            if 0 != p[1].returncode:
+                                print("\033[31mError in: " + p[0] + "\033[0m")
+                                print("\033[37m" + str(se.strip()) + str(output.strip()) + "\033[0m")
+                            else:
+                                output = se.strip()
+                                print("\033[37m" + os.path.basename(p[0]) + " pushed *\n" + output.strip() + "\033[0m")
                     else:
                         cnt += 1
-
-    for p in procs:
-
-        output, se = p[1].communicate()
-        if output:
-            output = output.decode("utf-8")
-        if se:
-            se = se.decode("utf-8")
-
-        if 0 != p[1].returncode:
-            print("\033[31mError in: " + p[0] + "\033[0m")
-            print("\033[37m" + str(se.strip()) + str(output.strip()) + "\033[0m")
-        else:
-            output = se.strip()
-            print("\033[37m" + os.path.basename(p[0]) + " pushed *\n" + output.strip() + "\033[0m")
 
 
 if __name__ == "__main__":
