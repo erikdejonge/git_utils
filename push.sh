c184de4209ea99db958fd6672d2d0beefa20fcf8
Erik de Jonge <erik@a8.nl>
2015-05-01 15:16:07 +0200

    documentation Friday 01 May 2015 (week:17 day:121), 15:16:05

--- push.sh
+++ push.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 python3 ~/workspace/git_utils/push_all_git_folders.py
-
 python3 ~/workspace/git_utils/status_all_git_folders.py
 
-
+python3 ~/workspace/git_utils/push_all_git_folders.py
+python3 ~/workspace/git_utils/status_all_git_folders.py
