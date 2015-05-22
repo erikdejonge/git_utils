0940ab170718b36ae559d7900e21197d5b45fc24
Erik de Jonge <erik@a8.nl>
2015-05-22 08:35:04 +0200

    git_utils Friday 22 May 2015 (week:20 day:142), 08:35:04

--- push.sh
+++ push.sh
@@ -3,8 +3,9 @@
 python3 ~/workspace/git_utils/push_all_git_folders.py
 
 if [ -e ".git/config" ]; then
-  echo "\033[0;90m"
+  echo "status cwd:\033[0;33m"
   pwd
+  echo "\033[0;90m "
   git status
   echo "\033[0m"
 fi
