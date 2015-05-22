8b1d6b18ff57d7bbfebd537468c2398484435548
Erik de Jonge <erik@a8.nl>
2015-05-22 08:38:31 +0200

    -

--- push.sh
+++ push.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-
-python3 ~/workspace/git_utils/push_all_git_folders.py
+#python3 ~/workspace/git_utils/push_all_git_folders.py
 
 if [ -e ".git/config" ]; then
-  echo "\033[0;33mstatus cwd: `pwd`"
-
-  echo "\033[0;90m "
-  git status
-  echo "\033[0m"
+  resultstatus=`git status`
+  if [[ $resultstatus == *"git add"* ]]
+  then
+      echo "\033[0;33mstatus cwd: `pwd`"
+      echo "\033[0;90m$resultstatus\033[0m"
+  fi
 fi
