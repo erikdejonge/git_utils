d687fdb8d82a595ffa5bd5d2138ae162846304bf
Erik de Jonge <erik@a8.nl>
2015-06-01 16:54:41 +0200

    Evita Peron: I am my own woman.
    Monday 01 June 2015 (week:22 day:152), 16:54:40

--- commit_all_git_folders_fast.py
+++ commit_all_git_folders_fast.py
@@ -25,7 +25,7 @@ def main():
     args, unknown = parser.parse_known_args()
 
     if args.message is None:
-        args.message = str(os.path.basename(os.getcwd())) + "\n" + str(timestamp)
+        args.message = str(os.popen("python3 ~/workspace/brainyquote/printbrainyquote.py -c -l 30 -r -d ~/workspace/brainyquote/quotes;").read()) + "\n" + str(timestamp)
 
     excludes = []
 
