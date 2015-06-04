160e4e1703ca665c65316efc92c1f90202002e8a
Erik de Jonge <erik@a8.nl>
2015-06-04 15:16:54 +0200

    Evita Peron: I am my own woman. Thursday 04 June 2015 (week:22 day:155), 15:16:53

--- commitpush.sh
+++ commitpush.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-stats=$(python3 ~/workspace/git_utils/status_all_git_folders.py -n)
+stats=$(python3 ~/workspace/git_utils/status_all_git_folders.py -n) &
 ~/workspace/git_utils/commitfast.sh;
 ~/workspace/git_utils/push.sh;
+wait
 echo $stats
