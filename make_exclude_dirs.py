dfe1ffd28a2cb6cd4a5a5616679e5ebad6979390
Erik de Jonge <erik@a8.nl>
2015-06-10 18:39:41 +0200

    git_utils:Wednesday 10 June 2015 (week:23 day:161), 18:39:03

--- make_exclude_dirs.py
+++ make_exclude_dirs.py
@@ -1,71 +1,69 @@
+#!/usr/bin/env python3
 # coding=utf-8
 """
 do not include the projects from the github directory for push
 """
-from __future__ import unicode_literals
-from __future__ import print_function
-from __future__ import division
-from __future__ import absolute_import
-from builtins import open
+from __future__ import division, print_function, absolute_import, unicode_literals
 from future import standard_library
-standard_library.install_aliases()
 
 import os
 import sys
+
+
+def print_stdout(chara, cnt=0, moddiv=1):
+    """
+    @type chara: str
+    @return: None
+    """
+    cnt += 1
+
+    if cnt % moddiv == 0:
+        sys.stdout.write(chara)
+        sys.stdout.flush()
+
+    return cnt
 
 
 def main():
     """
     main
     """
-    gen_list = write_file = True
-    githubfolder = os.path.expanduser("~") + "/workspace/github"
-    set_projects = set()
-    excludes = []
-    if os.path.exists(os.path.expanduser("~") + "/workspace/.gitutilsexclude"):
-        excludes.extend([os.path.join(os.path.expanduser("~") + "/workspace", x.strip()) for x in open(os.path.expanduser("~") + "/workspace/.gitutilsexclude").read().split("\n") if x.strip()])
-
-
-    if gen_list:
-        lastroot = ""
-
-        for root, dirlist, file in os.walk(githubfolder):
-            if root.count("/") < 7:
-                set_projects.add(root.replace(os.path.expanduser("~") + "/workspace/github/", ""))
-
-                for d in dirlist:
-                    set_projects.add(d.replace(os.path.expanduser("~") + "/workspace/github/", ""))
-    else:
-        set_projects = [projectname.strip() for projectname in open(os.path.expanduser("~") + "/workspace/git_utils/exclude_dirs").read().split("\n") if projectname.strip()]
-
     forks = set()
+    write_file = True
+    projects_set = set()
+    excludes = []
+    workspacefolder = os.path.join(os.path.expanduser("~"), "workspace")
 
-    for root, dirlist, files in os.walk(os.path.expanduser("~") + "/workspace"):
-        if not root.startswith(githubfolder) and root.endswith(".git") and not root in excludes:
-            # for afile in files:
-            #    print(afile)
-            project_dir = root.strip("/.git/")
-            project_base_folder = os.path.basename(os.path.dirname(project_dir))
-            if project_base_folder == "forks":
-                project_name = os.path.basename(project_dir)
+    if os.path.exists(os.path.join(workspacefolder, ".gitutilsexclude")):
+        excludes.extend([os.path.join(workspacefolder, x.strip()) for x in open(os.path.join(workspacefolder, ".gitutilsexclude")).read().split("\n") if x.strip()])
 
-                for project_name_iter in set_projects:
-                    if project_name in project_name_iter:
-                        forks.add(project_name_iter)
-        else:
-            print(root)
+    wsfolders = [os.path.join(workspacefolder, folder) for folder in os.listdir(workspacefolder) if os.path.join(workspacefolder, folder) not in excludes]
+    for wsfolder in wsfolders:
+        for root, dirlist, files in os.walk(wsfolder):
+            if root.endswith(".git"):
+                print_stdout(".")
+                project_dir = root.strip("/.git/")
+                project_base_folder = os.path.basename(os.path.dirname(project_dir))
+                if project_base_folder == "forks":
+                    project_name = os.path.basename(project_dir)
+
+                    for project_name_iter in projects_set:
+                        if project_name in project_name_iter:
+                            forks.add(project_name_iter)
+
     for fork in forks:
-        set_projects.remove(fork)
-
-    set_projects.remove("documentation")
+        projects_set.remove(fork)
 
     if write_file:
         afile = open(os.path.expanduser("~") + "/workspace/git_utils/exclude_dirs", "wt")
 
-        for projectname in set_projects:
+        for projectname in projects_set:
             afile.write(str(projectname) + "\n")
 
         afile.close()
+    print()
+
+standard_library.install_aliases()
 
 
 if __name__ == "__main__":
