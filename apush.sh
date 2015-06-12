e7efab7a92e5aa32bbf2f0929a0d2d455ec0d8c8
Erik de Jonge <erik@a8.nl>
2015-06-12 14:02:50 +0200

    Barbara Bush: My health is very good. Friday 12 June 2015 (week:23 day:163), 14:02:50

--- apush.sh
+++ apush.sh
@@ -4,3 +4,4 @@ python3 ~/workspace/git_utils/status_all_git_folders.py -f
 python3 ~/workspace/git_utils/commit_all_git_folders_fast.py
 python3 ~/workspace/git_utils/push_all_git_folders.py
 
+python3 ~/workspace/git_utils/status_all_git_folders.py
