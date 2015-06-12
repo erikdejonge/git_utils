661b312ae01e9b88328e9413e8f454ecf7414d62
Erik de Jonge <erik@a8.nl>
2015-06-12 14:03:22 +0200

    Jack Benny: Gags die, humor doesnt. Friday 12 June 2015 (week:23 day:163), 14:03:22

--- push.sh
+++ push.sh
@@ -1,13 +1,4 @@
 #!/bin/sh
 
 python3 ~/workspace/git_utils/push_all_git_folders.py
-
-if [ -e ".git/config" ]; then
-  resultstatus=`git status`
-  if [[ $resultstatus != *"nothing to commit, working directory clean"* ]]; then
-      echo
-  else
-      echo "\033[0;33mstatus cwd: `pwd`"
-      echo "\033[0;90m$resultstatus\033[0m"
-  fi
-fi
+python3 ~/workspace/git_utils/status_all_git_folders.py
