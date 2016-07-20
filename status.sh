e5db32673df91ede7b7d721b8f20ec2654776f52
Erik de Jonge <erik@a8.nl>
2016-07-20 23:04:11 +0200

    Wednesday 20 July 2016 (week:29 day:202), 23:04:11

--- status.sh
+++ status.sh
@@ -1,3 +1,7 @@
-#!/bin/sh
 
-python3 ~/workspace/git_utils/status_all_git_folders.py
+#!/bin/sh
+function globalpython3 () {
+    /Users/rabshakeh/.pyenv/versions/3.5.2/bin/python $@
+}
+
+globalpython3 ~/workspace/git_utils/status_all_git_folders.py
