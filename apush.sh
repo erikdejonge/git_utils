069bb45016dbaefcfb5adfc7be4f9e1c13f865aa
Erik de Jonge <erik@a8.nl>
2016-07-20 23:13:35 +0200

    Wednesday 20 July 2016 (week:29 day:202), 23:13:35

--- apush.sh
+++ apush.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
+function globalpython3 () {
+    /Users/rabshakeh/.pyenv/versions/3.5.2/bin/python $@
+}
 
-python3 ~/workspace/git_utils/status_all_git_folders.py -f
-python3 ~/workspace/git_utils/commit_all_git_folders_fast.py
-python3 ~/workspace/git_utils/push_all_git_folders.py
-python3 ~/workspace/git_utils/status_all_git_folders.py
+globalpython3 ~/workspace/git_utils/status_all_git_folders.py -f
+globalpython3 ~/workspace/git_utils/commit_all_git_folders_fast.py
+globalpython3 ~/workspace/git_utils/push_all_git_folders.py
+globalpython3 ~/workspace/git_utils/status_all_git_folders.py
